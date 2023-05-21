832e538a58d2279010d2a2903f0117ad3394b851
Alikhan <astanaproteam123@gmail.com>
2023-05-21 19:22:13 +0600

    main

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+package src;
+
+public class Main {
+    public static void main(String[] args) {
+        BST<Integer, String> myBST = new BST<>();
+        myBST.put(6 , "Gavi");
+        myBST.put(2, "Bellerin");
+        myBST.put(3, "Pique");
+        myBST.put(5,"Busquets");
+        myBST.put(1, "Ter Stegen");
+        myBST.put(4 , "Araujo");
+
+        System.out.println(myBST.get(2));
+        myBST.delete(2);
+        System.out.println(myBST.get(2));
+
+        System.out.println(myBST.iterator());
+    }
+}
